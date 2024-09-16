9d727e0ffae890e9cbd30053cce11f05f2d8b47f
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2024-09-16 15:36:24 +0100

    Added Requirements section

---
+++ src/components/RequirementsComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <hr />
+    <div class="text-3xl text-center p-2">
+      What you need to transform your physique?
+    </div>
+    <div class="p-4 flex justify-around text-xl">
+      <div class="text-center">
+        <div class="font-bold mb-3">Mindset</div>
+        <div>50%</div>
+      </div>
+      <div class="text-center text-bold">+</div>
+      <div class="text-center">
+        <div class="font-bold mb-3">Diet</div>
+        <div>20%</div>
+      </div>
+      <div class="text-center text-bold">+</div>
+      <div class="text-center">
+        <div class="font-bold mb-3">Workout</div>
+        <div>20%</div>
+      </div>
+      <div class="text-center text-bold">+</div>
+      <div class="text-center">
+        <div class="font-bold mb-3">Sleep</div>
+        <div>10%</div>
+      </div>
+    </div>
+    <hr />
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import HeroComponent from "@/components/HeroComponent.vue";
+import RequirementsComponent from "@/components/RequirementsComponent.vue";
 import DiscoverComponent from "@/components/DiscoverComponent.vue";
 </script>
 
 <template>
-  <div class="space-y-3">
+  <div class="space-y-5">
     <HeroComponent />
-    <div class="container mx-auto sm:px-6 lg:px-8">
+    <div class="container mx-auto sm:px-6 lg:px-8 space-y-5">
+      <RequirementsComponent />
       <DiscoverComponent />
     </div>
   </div>
