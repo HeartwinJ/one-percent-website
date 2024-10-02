c0245644ef1b41c378729b7d7ebc28805bb3bbff
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2024-10-02 18:23:13 +0100

    Added offerings component

---
+++ src/components/OfferingsComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <div class="text-3xl flex items-center justify-between py-4">
+      <div>Explore Our Offerings</div>
+      <button class="text-base bg-black rounded-lg p-2">View All</button>
+    </div>
+    <div class="grid grid-cols-2 gap-8 pt-4">
+      <div
+        v-for="(offering, idx) in [
+          'Strength',
+          'Cardio',
+          'Yoga',
+          'Nutrition',
+          'Personalized',
+          'Mindfulness',
+        ]"
+        :key="idx"
+        class="flex items-center gap-6 text-2xl"
+      >
+        <div>{{ idx + 1 }}</div>
+        <div class="grow h-0.5 bg-white/30 rounded-full"></div>
+        <div>{{ offering }}</div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import HeroComponent from "@/components/HeroComponent.vue";
 import RequirementsComponent from "@/components/RequirementsComponent.vue";
 import DiscoverComponent from "@/components/DiscoverComponent.vue";
+import OfferingsComponent from "@/components/OfferingsComponent.vue";
 </script>
 
 <template>
@@ -10,6 +11,7 @@ import DiscoverComponent from "@/components/DiscoverComponent.vue";
     <div class="container mx-auto sm:px-6 lg:px-8 space-y-5">
       <RequirementsComponent />
       <DiscoverComponent />
+      <OfferingsComponent />
     </div>
   </div>
 </template>
